39af6228b40718a8c8d754bc08dc90485fe06c06
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-01-15 21:16:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-//complete this code
-class Person {}
+class Person:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
 
-class Student extends Person {}
+    @property
+    def name(self):
+        return self._name
 
-class Teacher extends Person {}
+    @property
+    def age(self):
+        return self._age
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+    @age.setter
+    def age(self, value):
+        self._age = value
+
+
+class Student(Person):
+    def study(self):
+        print(f'{self.name} is studying')
+
+
+class Teacher(Person):
+    def teach(self):
+        print(f'{self.name} is teaching')
+
+
+# Example usage:
+person1 = Person("John", 30)
+print(f"Person 1: {person1.name}, Age: {person1.age}")
+
+student1 = Student("Alice", 20)
+print(f"Student 1: {student1.name}, Age: {student1.age}")
+student1.study()
+
+teacher1 = Teacher("Mr. Smith", 40)
+print(f"Teacher 1: {teacher1.name}, Age: {teacher1.age}")
+teacher1.teach()
+
