db1f164d057d3c637840499d7990166b3c71d42b
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-01-15 21:19:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
-class Person:
-    def __init__(self, name, age):
-        self._name = name
-        self._age = age
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-    @property
-    def name(self):
-        return self._name
+  get name() {
+    return this._name;
+  }
 
-    @property
-    def age(self):
-        return self._age
+  get age() {
+    return this._age;
+  }
 
-    @age.setter
-    def age(self, value):
-        self._age = value
+  set age(value) {
+    this._age = value;
+  }
+}
 
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
 
-class Student(Person):
-    def study(self):
-        print(f'{self.name} is studying')
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
-
-class Teacher(Person):
-    def teach(self):
-        print(f'{self.name} is teaching')
-
-
-# Example usage:
-person1 = Person("John", 30)
-print(f"Person 1: {person1.name}, Age: {person1.age}")
-
-student1 = Student("Alice", 20)
-print(f"Student 1: {student1.name}, Age: {student1.age}")
-student1.study()
-
-teacher1 = Teacher("Mr. Smith", 40)
-print(f"Teacher 1: {teacher1.name}, Age: {teacher1.age}")
-teacher1.teach()
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
 
